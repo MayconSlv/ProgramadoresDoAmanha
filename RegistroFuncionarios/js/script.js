b84d6431e5a1301aea512941ffe21e294146f597
Maycon Silva <mayconslv.contato@gmail.com>
2023-04-12 03:15:29 -0300

    Script de Registrar funcionário

--- RegistroFuncionarios/js/script.js
+++ RegistroFuncionarios/js/script.js
@@ -1,3 +1,5 @@
+// função aparecer e sumir o modal
+
 const openModal = document.querySelector('#openModal')
 const registerButton = document.querySelector('#register')
 const modal = document.querySelector('#modal')
@@ -11,3 +13,40 @@ const toggleModal = () => {
 ;[openModal, registerButton, fade].forEach((el) => {
   el.addEventListener('click', () => toggleModal())
 })
+
+//funçao armazenar os valores dos inputs
+
+let funcionarios = []
+
+function criarFuncionario(funcionario) {
+  const tbody = document.querySelector('#tbody')
+  const tr = document.createElement('tr')
+
+  tbody.appendChild(tr)
+
+  for (let i in funcionario) {
+    return (tr.innerHTML = `
+      <td>${funcionario[i].nome}</td>
+      <td>${funcionario[i].cargo}</td>
+      <td>${funcionario[i].salario}</td>
+      <td>${funcionario[i].telefone}</td>
+    `)
+  }
+}
+
+function registrarFuncionario() {
+  const name = document.querySelector('#name')
+  const role = document.querySelector('#role')
+  const salario = document.querySelector('#salario')
+  const tel = document.querySelector('#tel')
+
+  const funcionario = {
+    nome: name.value,
+    cargo: role.value,
+    salario: salario.value,
+    telefone: tel.value,
+  }
+
+  funcionarios.push(funcionario)
+  criarFuncionario(funcionarios)
+}
