ae912a0daf9dea1a547c682072fde158708a1d8f
Maycon Silva <mayconslv.contato@gmail.com>
2023-04-13 10:38:14 -0300

    Validando formularios com regex

--- RegistroFuncionarios/js/script.js
+++ RegistroFuncionarios/js/script.js
@@ -10,13 +10,13 @@ const toggleModal = () => {
   fade.classList.toggle('hide')
 }
 
-;[openModal, registerButton, fade].forEach((el) => {
+;[openModal, fade].forEach((el) => {
   el.addEventListener('click', () => toggleModal())
 })
 
 //funçao armazenar os valores dos inputs
 
-let funcionarios = []
+var funcionarios = []
 
 function criarFuncionario(funcionario) {
   const tbody = document.querySelector('#tbody')
@@ -25,28 +25,60 @@ function criarFuncionario(funcionario) {
   tbody.appendChild(tr)
 
   for (let i in funcionario) {
-    return (tr.innerHTML = `
+    tr.innerHTML = `
       <td>${funcionario[i].nome}</td>
       <td>${funcionario[i].cargo}</td>
       <td>${funcionario[i].salario}</td>
       <td>${funcionario[i].telefone}</td>
-    `)
+    `
   }
 }
 
-function registrarFuncionario() {
-  const name = document.querySelector('#name')
+function nameValidation(event) {
+  const input = event.currentTarget
+  const nameTest = regex.test(input.value)
+
+  if (!nameTest) {
+    registerButton.setAttribute('disabled', 'disabled')
+  } else {
+    registerButton.removeAttribute('disabled')
+  }
+}
+
+registerButton.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  const fullName = document.querySelector('#name')
   const role = document.querySelector('#role')
   const salario = document.querySelector('#salario')
   const tel = document.querySelector('#tel')
 
-  const funcionario = {
-    nome: name.value,
-    cargo: role.value,
-    salario: salario.value,
-    telefone: tel.value,
-  }
+  const nameRegex = /^[a-zA-ZáâãéêíóôõúçÁÂÉÊÍÓÔÕÚÇ ,.'-]+$/u
+  const telRegex =
+    /^(1[1-9]|[4689][0-9]|2[12478]|3([1-5]|[7-8])|5([13-5])|7[193-7])9[0-9]{8}$/
 
-  funcionarios.push(funcionario)
-  criarFuncionario(funcionarios)
-}
+  if (fullName.value === '' || !nameRegex.test(fullName.value)) {
+    alert('Preencha o nome')
+    return
+  } else if (role.value === '' || !nameRegex.test(role.value)) {
+    alert('Nome invalido')
+  } else if (tel.value === '' || !telRegex.test(tel.value)) {
+    alert('Numero invalido')
+    return
+  } else {
+    const funcionario = {
+      nome: fullName.value,
+      cargo: role.value,
+      salario: salario.value,
+      telefone: tel.value,
+    }
+
+    funcionarios.push(funcionario)
+    criarFuncionario(funcionarios)
+
+    fullName.value = ''
+    role.value = ''
+    salario.value = ''
+    tel.value = ''
+  }
+})
